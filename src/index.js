d761197cc2d99d5638e5090252f88406f297b61c
krhuber <kr.huber@gmail.com>
2022-08-02 14:51:08 -0400

    Added Date and Timestamp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
-function displayTemperature(response) {
-  console.log(response.data);
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let AmOrPm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
+  return `${day} - `;
+}
+
+function formatTimestamp(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let AmOrPm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return ` ${hours}:${minutes} ${AmOrPm}`;
+}
+
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let timestampElement = document.querySelector("#timestamp");
   let tempMaxElement = document.querySelector("#temp-max");
   let tempMinElement = document.querySelector("#temp-min");
 
@@ -16,6 +51,8 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   tempMaxElement.innerHTML = Math.round(response.data.main.temp_max);
   tempMinElement.innerHTML = Math.round(response.data.main.temp_min);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  timestampElement.innerHTML = formatTimestamp(response.data.dt * 1000);
 }
 let apiKey = "84101301d1e855bff406380c1296fbbf";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
