fb1647bb36711d84ed6d60ecdbcfa8cad8487eb8
krhuber <kr.huber@gmail.com>
2022-08-02 17:40:29 -0400

    Added relevant icon and icon images
    Used API to get current information/icon but replaced it with my own icons downloaded from https://www.flaticon.com/packs/weather-765?word=weather%20forecast

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//function to show current day of the week
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -21,6 +22,7 @@ function formatDate(timestamp) {
   return `${day} - `;
 }
 
+//function to show timestap next to current day of the week
 function formatTimestamp(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -32,7 +34,7 @@ function formatTimestamp(timestamp) {
   }
   return ` ${hours}:${minutes} ${AmOrPm}`;
 }
-
+// Main function for showing current conditions
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -43,6 +45,8 @@ function displayTemperature(response) {
   let timestampElement = document.querySelector("#timestamp");
   let tempMaxElement = document.querySelector("#temp-max");
   let tempMinElement = document.querySelector("#temp-min");
+  let iconElement = document.querySelector("#icon");
+  let iconElementApi = response.data.weather[0].icon;
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -53,8 +57,12 @@ function displayTemperature(response) {
   tempMinElement.innerHTML = Math.round(response.data.main.temp_min);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   timestampElement.innerHTML = formatTimestamp(response.data.dt * 1000);
+  iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
+  iconElement.setAttribute("src", `icons/${iconElementApi}.png`);
 }
+
 let apiKey = "84101301d1e855bff406380c1296fbbf";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
